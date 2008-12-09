9a4b18f1c494b0b43c5b0cfb11a5a0047de072f7
Eric Naeseth <enaeseth@gmail.com>
2008-12-09 14:59:12 -0600

    Adding the base smooth.php file.

---
+++ smooth.php
@@ -0,0 +1,39 @@
+<?php
+
+// SMOOTH: The PHP framework that goes down easy.
+// Copyright © 2008 Carleton College.
+
+// This file just contains some housekeeping; for the real meat see
+// lib/smooth.php.
+
+function path_join() {
+    $parts = func_get_args();
+    return implode(DIRECTORY_SEPARATOR, $parts);
+}
+
+define('SMOOTH_ROOT', dirname(__FILE__));
+function smooth_path() {
+    $parts = func_get_args();
+    return path_join(SMOOTH_ROOT, path_join($parts));
+}
+
+function _smooth_get_lib_path($library) {
+    return path_join(SMOOTH_ROOT, 'lib', 'smooth', "$library.php");
+}
+
+function _smooth_transform_path($path) {
+    return str_replace('/', DIRECTORY_SEPARATOR, $path);
+}
+
+function smooth_load() {
+    $libs = func_get_args();
+    if (DIRECTORY_SEPARATOR != '/')
+        $libs = array_map('_smooth_transform_path', $libs);
+    $paths = array_map('_smooth_get_lib_path', $libs);
+    foreach ($paths as $path) {
+        require_once $path;
+    }
+}
+
+// Let's get this started!
+require smooth_path('lib', 'smooth.php');
