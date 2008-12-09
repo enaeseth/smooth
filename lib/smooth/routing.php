7a0c21ef21677e0d02c92461eeeefe908e096db9
Eric Naeseth <enaeseth@gmail.com>
2008-12-09 15:42:19 -0600

    Adding base SmoothRouter class.

---
+++ lib/smooth/routing.php
@@ -0,0 +1,47 @@
+<?php
+
+// SMOOTH: The PHP framework that goes down easy.
+// Copyright © 2008 Carleton College.
+
+abstract class SmoothRouter {
+    protected $application;
+    
+    public function __construct(SmoothApplication $application) {
+        $this->application = $application;
+    }
+    
+    public abstract function route(SmoothRequest $request);
+    
+    protected function getRouteFile($extensions=null) {
+        if (!$extensions) {
+            $extensions = array('yml', 'yaml', 'conf', 'php', 'txt');
+        }
+        $extensions = implode(',', $extensions);
+        
+        $pattern = path_join($this->application->root, '{config,configuration}',
+            'routes.{'.$extensions.'}');
+        $files = glob($pattern, GLOB_BRACE);
+        
+        if (empty($files))
+            return null;
+        if (!is_readable($files[0])) {
+            throw new SmoothSetupException('Routes file "'.
+                $files[0].'" is not readable.');
+        }
+        return $files[0];
+    }
+    
+    public static function get($router, SmoothApplication $application) {
+        if ($router == 'default')
+            $router = 'pattern';
+        
+        switch ($router) {
+            case 'pattern':
+                smooth_load('routers/pattern');
+                return new SmoothPatternRouter($application);
+            default:
+                throw new SmoothSetupException('Unknown Smooth router '.
+                    '"'.$router.'".');
+        }
+    }
+}
