6c543098dbc63790412edd223b40a4728963e997
Eric Naeseth <enaeseth@gmail.com>
2008-12-09 14:59:29 -0600

    Adding the root Smooth library file.

---
+++ lib/smooth.php
@@ -0,0 +1,10 @@
+<?php
+
+// SMOOTH: The PHP framework that goes down easy.
+// Copyright © 2008 Carleton College.
+
+class Smooth {
+    public const VERSION = '0.1.0';
+}
+
+smooth_load('application');
