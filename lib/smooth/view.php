644db3b479493196ba5823148ae7cb4a9c75ef46
Eric Naeseth <enaeseth@gmail.com>
2008-12-12 09:55:04 -0600

    Adding a context tree to views; child views now inherit context from their parents.

--- lib/smooth/view.php
+++ lib/smooth/view.php
@@ -6,6 +6,8 @@
 class SmoothView {
     private $path;
     
+    private static $context_tree = array();
+    
     public function __construct($path) {
         $this->path = $path;
     }
@@ -15,17 +17,30 @@ class SmoothView {
     }
     
     public function render(&$context) {
+        foreach (self::$context_tree as &$older_context) {
+            if (is_array($older_context)) {
+                extract($older_context, EXTR_OVERWRITE | EXTR_REFS);
+            } else {
+                foreach ($older_context as $k => $v) {
+                    $$k = &$older_context->$k;
+                }
+            }
+            
+        }
+        
         if (is_array($context)) {
             extract($context, EXTR_OVERWRITE | EXTR_REFS);
         } else if (is_object($context)) {
             foreach ($context as $k => $v) {
-                $$k = $v;
+                $$k = &$context->$k;
             }
         } else {
             throw new InvalidArgumentException('Rendering context must be '.
                 'an array or an object.');
         }
         
+        self::$context_tree[] =& $context;
         include $this->path;
+        array_pop(self::$context_tree);
     }
 }
